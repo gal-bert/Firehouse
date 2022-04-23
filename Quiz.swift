903ebab2b23482b0c60c5429876769a14d1e08eb
Gregorius Albert <galbert.djaja@gmail.com>
2022-04-23 21:56:06 +0700

    Initial Commit

---
+++ Quiz.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  firehouse
+//
+//  Created by Gregorius Albert on 23/04/22.
+//
+
+import SwiftUI
+
+struct Quiz:View {
+    var body: some View {
+        ZStack {
+            Color.red.ignoresSafeArea()
+        }
+    }
+}
---
+++ StartScreen.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+
+let BLUE_COLOR = UIColor(hue: 222/360, saturation: 100/100, brightness: 62/100, alpha: 1.0)
+
+struct StartScreen: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color(BLUE_COLOR)
+                    .ignoresSafeArea()
+                VStack {
+                    Image("fire")
+                        .resizable()
+                        .frame(width: 250, height: 250, alignment: .center)
+                        .aspectRatio(contentMode: .fill)
+                        .cornerRadius(20)
+                        .padding(.bottom).padding(.bottom)
+                    
+                    Text("Firehouse")
+                        .foregroundColor(.orange)
+                        .font(.system(size: 40))
+                        .padding(.bottom)
+                    
+                    Text("🔥 + 🧯 = ✅")
+                        .foregroundColor(.orange)
+                        .font(.system(size: 40))
+                        .padding(.bottom)
+                        .padding(.bottom)
+                        .padding(.bottom)
+                    
+                    NavigationLink(destination: Home()) {
+                        Text("Get Started")
+                            .frame(width: 200, height: 50, alignment: .center)
+                            .background(Color.orange)
+                            .foregroundColor(Color.white)
+                            .cornerRadius(10)
+                    }
+                    
+                }
+            }
+        }
+    }
+}
